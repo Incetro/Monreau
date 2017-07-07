e7d41a87ed0b8898236ff79c25cb385fdc755480
incetro <incetro@ya.ru>
2017-07-07 10:43:09 +0300

    Added SPM

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  Monreau
+//
+//  Created by incetro on 07/07/2017.
+//
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Monreau",
+    dependencies : [],
+    exclude: ["Tests"]
+)
